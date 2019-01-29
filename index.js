e66324e77801e282af8875711a5833e5fedf35a6
Will Munslow <will@subterrane.com>
2019-01-29 13:58:38 -0800

    grep-like component usage finder

--- index.js
+++ index.js
@@ -4,12 +4,21 @@ const program = require("commander");
 const gitignore = require("ignored");
 const babel = require("@babel/core");
 const ignore = require("ignore");
+const grepit = require("grepit");
 const chalk = require("chalk");
 const find = require("find");
 const path = require("path");
 const fs = require("fs");
 
-program.version("1.0.0").parse(process.argv);
+let componentName;
+
+program
+  .version("1.0.0")
+  .arguments("[component]")
+  .action(function(component) {
+    componentName = component;
+  })
+  .parse(process.argv);
 
 //
 // probably need some options here.
@@ -49,31 +58,41 @@ find
     let filtered = ig.filter(files); // filter out the files in the gitignore
 
     filtered.forEach(file => {
-      fs.readFile(file, (err, data) => {
-        if (err) return displayError(err);
-
-        babel.parse(data, opts.options, function(err, ast) {
+      if (componentName) {
+        const components = grepit(`<${componentName}`, file);
+        if (components.length) {
+          console.log(chalk.bold(path.join(process.cwd(), file)));
+          components.forEach(line =>
+            console.log(chalk.green("\t-", line.trim()))
+          );
+        }
+      } else {
+        fs.readFile(file, (err, data) => {
           if (err) return displayError(err);
 
-          let components = [];
-          babel.traverse(ast, {
-            ClassDeclaration: function(path) {
-              if (
-                path.node.superClass &&
-                path.node.superClass.name === "Component"
-              ) {
-                components.push(path.node.id.name);
-                //console.dir(path.node, { depth: 10 });
+          babel.parse(data, opts.options, function(err, ast) {
+            if (err) return displayError(err);
+
+            let components = [];
+            babel.traverse(ast, {
+              ClassDeclaration: function(path) {
+                if (
+                  path.node.superClass &&
+                  path.node.superClass.name === "Component"
+                ) {
+                  components.push(path.node.id.name);
+                  //console.dir(path.node, { depth: 10 });
+                }
               }
+            });
+
+            if (components.length) {
+              console.log(chalk.bold(path.join(process.cwd(), file)));
+              components.forEach(name => console.log(chalk.green("\t-", name)));
             }
           });
-
-          if (components.length) {
-            console.log(chalk.bold(path.join(process.cwd(), file)));
-            components.forEach(name => console.log(chalk.green("\t-", name)));
-          }
         });
-      });
+      }
     });
   })
   .error(function(err) {
@@ -82,5 +101,5 @@ find
 
 function displayError(err) {
   console.error(err.message);
-  return 1;
+  process.exit(1);
 }
